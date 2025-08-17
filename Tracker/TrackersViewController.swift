2ed1a5f207004b0db9103c23bb610fdbe1bfff25
soulhidee <serverpgig@yandex.ru>
2025-08-17 17:29:30 +0300

    feat: Add UISearchBar with placeholder fix: Fix constraints

--- Tracker/TrackersViewController.swift
+++ Tracker/TrackersViewController.swift
@@ -5,14 +5,15 @@ final class TrackersViewController: UIViewController {
     // MARK: - UI Elements
     private lazy var topStack: UIStackView = {
         let stack = UIStackView()
+        stack.axis = .horizontal
         stack.alignment = .center
-        stack.distribution = .equalSpacing
+        stack.distribution = .equalCentering
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
     
     private lazy var addButton: UIButton = {
-        let button = UIButton(type: .system)
+        let button = UIButton(type: .custom)
         let image = UIImage.addTracker.withRenderingMode(.alwaysTemplate)
         button.setImage(image, for: .normal)
         button.tintColor = UIColor.ypBlack
@@ -26,7 +27,7 @@ final class TrackersViewController: UIViewController {
         config.title = "14.12.22"
         config.baseForegroundColor = UIColor.ypTextBlack
         config.contentInsets = NSDirectionalEdgeInsets(top: 6, leading: 5.5, bottom: 6, trailing: 5.5)
-
+        
         let button = UIButton(configuration: config, primaryAction: nil)
         button.backgroundColor = UIColor.ypBackgroundLight
         button.layer.cornerRadius = 8
@@ -45,6 +46,14 @@ final class TrackersViewController: UIViewController {
         return label
     }()
     
+    private lazy var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.placeholder = "Поиск"
+        bar.searchBarStyle = .minimal
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        return bar
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,17 +68,23 @@ final class TrackersViewController: UIViewController {
         topStack.addArrangedSubview(addButton)
         topStack.addArrangedSubview(dataButton)
         view.addSubview(titleLabel)
+        view.addSubview(searchBar)
     }
     
     // MARK: - Constraints
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            topStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            topStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 3),
+            topStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 1),
+            topStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 6),
             topStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             
-            titleLabel.topAnchor.constraint(equalTo: topStack.bottomAnchor, constant: 5),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+            titleLabel.topAnchor.constraint(equalTo: topStack.bottomAnchor, constant: 1),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            searchBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 7),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            searchBar.heightAnchor.constraint(equalToConstant: 36)
         ])
     }
     
