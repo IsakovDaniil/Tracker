38c615eee0edb8bd336499406fb2fdc3edbb4181
soulhidee <serverpgig@yandex.ru>
2025-08-17 13:55:30 +0300

    feat(TrackersViewController): Add topStack UIStackView

--- Tracker/TrackersViewController.swift
+++ Tracker/TrackersViewController.swift
@@ -1,7 +1,15 @@
 import UIKit
 
 final class TrackersViewController: UIViewController {
-
+    
+    private lazy var topStack: UIStackView = {
+        let stack = UIStackView()
+        stack.alignment = .center
+        stack.distribution = .equalSpacing
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.ypWhite
