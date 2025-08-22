2c663965c3fb73adad7fa25483438680d388c7a1
soulhidee <serverpgig@yandex.ru>
2025-08-22 21:04:18 +0300

    feat(AddModalViewController): Add vertical buttons stack view with constraints

--- Tracker/ModalsView/AddModalViewController.swift
+++ Tracker/ModalsView/AddModalViewController.swift
@@ -4,6 +4,15 @@ final class AddModalViewController: UIViewController {
     //MARK: - UI Elements
     private lazy var titleLabel = UILabel.ypTitle("Создание трекера")
     
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +28,17 @@ final class AddModalViewController: UIViewController {
         view.backgroundColor = UIColor.ypWhite
         view.layer.cornerRadius = 12
         view.addSubview(titleLabel)
+        view.addSubview(buttonsStackView)
     }
     
     // MARK: - Setup Constraints
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 27),
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            buttonsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
     
