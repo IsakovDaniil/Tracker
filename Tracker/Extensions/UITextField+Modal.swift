40b811743715ee5e0ac92a1493ba351b353c3eb1
soulhidee <serverpgig@yandex.ru>
2025-08-26 17:40:40 +0300

    feat: Add custom modal text field factory

---
+++ Tracker/Extensions/UITextField+Modal.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+extension UITextField {
+    static func ypAddModalTextField() -> UITextField {
+        let textField = UITextField()
+        textField.textColor = UIColor.ypBlack
+        textField.backgroundColor = UIColor.ypBackground
+        textField.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        textField.placeholder = "Введите название трекера"
+        textField.layer.cornerRadius = 15
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.clearButtonMode = .whileEditing
+
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 17, height: 0))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+
+        return textField
+    }
+}
--- Tracker/ModalsView/NewEventModalViewController.swift
+++ Tracker/ModalsView/NewEventModalViewController.swift
@@ -4,7 +4,21 @@ final class NewEventModalViewController: UIViewController {
     //MARK: - UI Elements
     private lazy var titleLabel = UILabel.ypTitle("Новое нерегулярное событие")
     
-
+    private lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = UIColor.ypBlack
+        textField.backgroundColor = UIColor.ypBackground
+        textField.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        textField.placeholder = "Введите название трекера"
+        textField.layer.cornerRadius = 15
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.clearButtonMode = .whileEditing
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 17, height: 0))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -20,6 +34,7 @@ final class NewEventModalViewController: UIViewController {
         view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         view.backgroundColor = UIColor.ypWhite
         view.addSubview(titleLabel)
+        view.addSubview(nameTextField)
     }
     
     // MARK: - Setup Constraints
@@ -28,10 +43,13 @@ final class NewEventModalViewController: UIViewController {
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 27),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
-
+            nameTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 38),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            nameTextField.heightAnchor.constraint(equalToConstant: 75)
         ])
     }
     
     // MARK: - Action
-
+    
 }
