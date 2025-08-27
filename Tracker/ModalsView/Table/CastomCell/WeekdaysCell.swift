0f579ef09a5719a29c5e128e1ce37a58c25093e2
soulhidee <serverpgig@yandex.ru>
2025-08-27 16:56:31 +0300

    feat(WeekdaysCell): add reusable cell with label and switch for weekdays

--- Tracker/ModalsView/Table/CastomCell/WeekdaysCell.swift
+++ Tracker/ModalsView/Table/CastomCell/WeekdaysCell.swift
@@ -1,8 +1,39 @@
-//
-//  WeekdaysCell.swift
-//  Tracker
-//
-//  Created by Даниил on 27.08.2025.
-//
+import UIKit
 
-import Foundation
+class WeekdaysCell: UITableViewCell {
+    
+    private let dayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let toggleSwitch: UISwitch = {
+        let switchView = UISwitch()
+        switchView.translatesAutoresizingMaskIntoConstraints = false
+        return switchView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(dayLabel)
+        contentView.addSubview(toggleSwitch)
+
+        NSLayoutConstraint.activate([
+            dayLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            dayLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+
+            toggleSwitch.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            toggleSwitch.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with title: String, isOn: Bool) {
+        dayLabel.text = title
+        toggleSwitch.isOn = isOn
+    }
+}
