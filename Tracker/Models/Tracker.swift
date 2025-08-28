e6610c8493b134edf9cfae7637b8608ebf4032a3
soulhidee <serverpgig@yandex.ru>
2025-08-28 18:02:34 +0300

    refactor: Move Weekday enum to a separate file

--- Tracker/Models/Tracker.swift
+++ Tracker/Models/Tracker.swift
@@ -8,18 +8,3 @@ struct Tracker {
     let schedule: [Weekday]
 }
 
-enum Weekday: Int, CaseIterable {
-    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
-    
-    var sortName: String {
-        switch self {
-        case .monday: return "Пн"
-        case .tuesday: return "Вт"
-        case .wednesday: return "Ср"
-        case .thursday: return "Чт"
-        case .friday: return "Пт"
-        case .saturday: return "Сб"
-        case .sunday: return "Вс"
-        }
-    }
-}
---
+++ Tracker/Models/Weekday.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum Weekday: Int, CaseIterable {
+    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+    
+    var sortName: String {
+        switch self {
+        case .monday: return "Пн"
+        case .tuesday: return "Вт"
+        case .wednesday: return "Ср"
+        case .thursday: return "Чт"
+        case .friday: return "Пт"
+        case .saturday: return "Сб"
+        case .sunday: return "Вс"
+        }
+    }
+}
