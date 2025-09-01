805bd876984f2499b77e3d43e1e5d663865cc352
soulhidee <serverpgig@yandex.ru>
2025-09-01 21:42:05 +0300

    feat: Extract AddTrackersModal constants to separate file

--- Tracker/ModalsView/AddTrackersModalViewController.swift
+++ Tracker/ModalsView/AddTrackersModalViewController.swift
@@ -6,24 +6,6 @@ protocol AddTrackersModalDelegate: AnyObject {
 }
 
 final class AddTrackersModalViewController: UIViewController {
-    // MARK: - Constants
-    private enum AddTrackersConstants {
-        struct Layout {
-            static let cornerRadius: CGFloat = 10
-            static let titleTopInset: CGFloat = 27
-            static let stackSpacing: CGFloat = 16
-            static let stackLeadingInset: CGFloat = 16
-            static let stackTrailingInset: CGFloat = -16
-            static let buttonHeight: CGFloat = 60
-        }
-        
-        struct Strings {
-            static let titleText = "Создание трекера"
-            static let habitButtonTitle = "Привычка"
-            static let eventButtonTitle = "Нерегулярное событие"
-        }
-    }
-    
     // MARK: - Properties
     weak var delegate: AddTrackersModalDelegate?
     
