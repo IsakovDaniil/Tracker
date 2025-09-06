5af93db5007f2b957bc07a9fe56611bd3ebbf5b8
soulhidee <serverpgig@yandex.ru>
2025-09-06 14:46:43 +0300

    refactor(ColorCell): Redesign cell with color view and selection border

--- Tracker/CollectionCell/ColorCell.swift
+++ Tracker/CollectionCell/ColorCell.swift
@@ -4,10 +4,21 @@ final class ColorCell: UICollectionViewCell {
     
     static let reuseIdentifier = "ColorCell"
     
+    // MARK: UI Element
+    private let colorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 8
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupCell()
+        setupView()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -19,13 +30,27 @@ final class ColorCell: UICollectionViewCell {
         layer.cornerRadius = 16
     }
     
+    private func setupView() {
+        contentView.addSubview(colorView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            colorView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 6),
+            colorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 6),
+            colorView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -6),
+            colorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -6)
+        ])
+    }
+    
     // MARK: - Configuration
     func configure(with color: UIColor, isSelected: Bool) {
-        backgroundColor = color
+        colorView.backgroundColor = color
         
         if isSelected {
             layer.borderWidth = 3
             layer.borderColor = color.cgColor
+            layer.borderColor = color.withAlphaComponent(0.3).cgColor
         } else {
             layer.borderWidth = .zero
         }
