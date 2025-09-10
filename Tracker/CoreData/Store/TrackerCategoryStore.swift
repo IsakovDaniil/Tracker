de2fb096a0d1603756f85c9349124d5d6f29974c
soulhidee <serverpgig@yandex.ru>
2025-09-10 14:26:13 +0300

    feat(TrackerCategoryStore): Add TrackerCategoryStore skeleton with protocol methods

--- Tracker/CoreData/Store/TrackerCategoryStore.swift
+++ Tracker/CoreData/Store/TrackerCategoryStore.swift
@@ -1,1 +1,37 @@
 import Foundation
+import CoreData
+
+protocol TrackerCategoryStoreProtocol {
+    func addCategory(_ category: TrackerCategory) throws
+    func fetchAllCategories() throws -> [TrackerCategory]
+    func deleteCategory(witchTitle title: String) throws
+    func findOrCreateCategory(with title: String) throws -> TrackerCategoryCoreData
+}
+
+final class TrackerCategoryStore: NSObject {
+    private let context: NSManagedObjectContext
+    
+    init(coreDataManager: CoreDataManager) {
+        self.context = coreDataManager.context
+    }
+}
+
+extension TrackerCategoryStore: TrackerCategoryStoreProtocol {
+    func addCategory(_ category: TrackerCategory) throws {
+        <#code#>
+    }
+    
+    func fetchAllCategories() throws -> [TrackerCategory] {
+        <#code#>
+    }
+    
+    func deleteCategory(witchTitle title: String) throws {
+        <#code#>
+    }
+    
+    func findOrCreateCategory(with title: String) throws -> TrackerCategoryCoreData {
+        <#code#>
+    }
+    
+    
+}
