35fd4b1cc5402ea5d24e2a4c0190619c857b9314
soulhidee <serverpgig@yandex.ru>
2025-09-25 13:33:55 +0300

    feat: Complete UI for AddCategoryModalViewController

--- Tracker/ModalsView/Категоря/AddCategoryModalViewController.swift
+++ Tracker/ModalsView/Категоря/AddCategoryModalViewController.swift
@@ -7,7 +7,7 @@ final class AddCategoryModalViewController: UIViewController {
     
     private let stubImageView = UIImageView.stubImage()
     
-    private let stubLabel = UILabel.stubLabel(withText: TrackersViewConstants.Strings.stubLabelText)
+    private let stubLabel = UILabel.stubLabel(withText: "Привычки и события можно объединить по смыслу")
     
     private lazy var addCategoryButton = UIButton.ypAddModalButton(
         title: "Добавить категорию",
@@ -23,11 +23,32 @@ final class AddCategoryModalViewController: UIViewController {
     }
     
     private func setupView() {
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = NewEventConstants.Layout.cornerRadius
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        view.backgroundColor = UIColor.ypWhite
         
+        view.addSubview(titleLabel)
+        view.addSubview(stubStack)
+        stubStack.addArrangedSubview(stubImageView)
+        stubStack.addArrangedSubview(stubLabel)
+        
+        view.addSubview(addCategoryButton)
     }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 27),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            stubStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stubStack.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 246),
+            
+            addCategoryButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            addCategoryButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            addCategoryButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            addCategoryButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
     }
     
     @objc private func addCategoryButtonTapped() {
