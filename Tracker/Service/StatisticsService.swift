c1c2600805101651c2f181b97f377a2d5e9b17c5
soulhidee <serverpgig@yandex.ru>
2025-10-08 15:36:10 +0300

    chore(StatisticsService): Prepare structure

--- Tracker/Service/StatisticsService.swift
+++ Tracker/Service/StatisticsService.swift
@@ -1,8 +1,30 @@
-//
-//  StatisicsService.swift
-//  Tracker
-//
-//  Created by Даниил on 08.10.2025.
-//
-
 import Foundation
+
+protocol StatisticsServiceProtocol {
+    func calculateStatistics() -> Statistics
+}
+
+
+final class StatisticsService: StatisticsServiceProtocol {
+    
+    // MARK: - Dependencies
+    private let trackerStore: TrackerStore
+    private let recordStore: TrackerRecordStore
+    private let calendar: Calendar
+    
+    
+    // MARK: - Init
+    init(
+        trackerStore: TrackerStore = TrackerStore(),
+        recordStore: TrackerRecordStore = TrackerRecordStore(),
+        calendar: Calendar = .current
+    ) {
+        self.trackerStore = trackerStore
+        self.recordStore = recordStore
+        self.calendar = calendar
+    }
+    
+    func calculateStatistics() -> Statistics {
+        <#code#>
+    }
+}
