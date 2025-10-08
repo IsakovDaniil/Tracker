19437cec5ffb393bd1360483ca705ae0514ce719
soulhidee <serverpgig@yandex.ru>
2025-10-08 15:07:43 +0300

    feat(Statistics): Add Statistics struct with zero state

--- Tracker/Models/Statistics.swift
+++ Tracker/Models/Statistics.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+struct Statistics {
+    let bestStreak: Int
+    let perfectDays: Int
+    let completedTrackers: Int
+    let averagePerDay: Int
+    
+    static let zero = Statistics(
+        bestStreak: .zero,
+        perfectDays: .zero,
+        completedTrackers: .zero,
+        averagePerDay: .zero
+    )
+}
